065bab970a0b5b814dc4c4b41cb248c96d0d328c
Walmir Neto <owalmirneto@gmail.com>
2024-02-14 11:14:31 -0300

    feat: create first version

--- spec/capy/spec_spec.rb
+++ spec/capy/spec_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe Capy::Spec do
   it "has a version number" do
-    expect(Capy::Spec::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(described_class::VERSION).not_to be_nil
   end
 end
