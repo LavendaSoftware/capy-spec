922ff82fd0c9a85fbc41803b062f52abceaaf670
Walmir Neto <owalmirneto@gmail.com>
2024-02-14 17:16:20 -0300

    feat: create class to load application

--- lib/capy_spec.rb
+++ lib/capy_spec.rb
@@ -4,4 +4,14 @@ require_relative "capy_spec/version"
 
 module CapySpec
   class Error < StandardError; end
+
+  class Setup
+    DEFAULT_FILES = ["capy_spec/all"]
+
+    class << self
+      def call(files = DEFAULT_FILES)
+        files.each { |file| require file }
+      end
+    end
+  end
 end
